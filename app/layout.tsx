e6ca1be2be078d3d53394ec81048f7ef086a7a82
christopher ha <63668974+christopher-ha@users.noreply.github.com>
2023-06-21 10:40:32 -0700

    refactor: Add metadata back incl. fix for input zoom on mobile
    Previous code in the /pages/ directory contained metadata, which includes the code to prevent zoom on mobile for the input. This has been converted to Next.js 13 next/head.
    
    Since the font-size is under 16px, Safari will zoom in on the input and makes it almost impossible to zoom out unless the user was pinching on the input field or UI on top.
    
    The metadata prevents the zoom and restores normal function.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,18 @@
 import "@/styles/globals.scss";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "𝓁𝒾𝓋𝒾𝓃𝑔 𝒹𝒾𝑔𝒾𝓉𝒶𝓁 𝒶𝓇𝒸𝒽𝒾𝓋𝑒𝓈",
+  description:
+    "An archive of the deleted websites on Yahoo! Geocities in the context of modern web frameworks—capturing the tension between a lost past and a distant future.",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  openGraph: {
+    title: "𝓁𝒾𝓋𝒾𝓃𝑔 𝒹𝒾𝑔𝒾𝓉𝒶𝓁 𝒶𝓇𝒸𝒽𝒾𝓋𝑒𝓈",
+    description:
+      "An archive of the deleted websites on Yahoo! Geocities in the context of modern web frameworks—capturing the tension between a lost past and a distant future.",
+    images: "https://living-digital-archives.bhris.digital/og_image.jpg",
+  },
+};
 
 export default function RootLayout({
   // Layouts must accept a children prop.
